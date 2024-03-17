ce56724a13a83a342be7f2afea869557284ff6b5
Claire Howard <142757298+cbear243@users.noreply.github.com>
2024-03-17 23:18:27 +0000

    Sun, Mar 17, 2024, 4:18 PM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -3,15 +3,19 @@ open = document.getElementById('open')
 modal = document.getElementById('modal')
 close = document.getElementById('close')
 
-// 
+// Toggle Nav
 toggle.addEventListener('click', () => {
   document.body.classList.toggle('show-nav')
 })
 
+// Show Modal
 open.addEventListener('click', () => {
   modal.classList.add('show-modal')
 })
 
+// Hide Modal
 close.addEventListener('click', () => {
     modal.classList.remove('show-modal')
 })
+
+//Hide Modal on outside click
